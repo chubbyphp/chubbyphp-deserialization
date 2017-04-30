f6e0241b36617728d23a11a9e1567215e422275a
Dominik Zogg <dominik.zogg@gmail.com>
2017-04-30 13:51:13 +0200

    remove all code related to deserialize Model

--- tests/DeserializerTest.php
+++ tests/DeserializerTest.php
@@ -5,170 +5,49 @@ namespace Chubbyphp\Tests\Deserialize\Deserializer;
 
 use Chubbyphp\Deserialize\Deserializer;
 use Chubbyphp\Deserialize\Registry\ObjectMappingRegistry;
-use Chubbyphp\Model\ResolverInterface;
-use Chubbyphp\Tests\Deserialize\Resources\Mapping\ManyMapping;
-use Chubbyphp\Tests\Deserialize\Resources\Mapping\OneMapping;
-use Chubbyphp\Tests\Deserialize\Resources\Mapping\ReferenceMapping;
-use Chubbyphp\Tests\Deserialize\Resources\Model\Many;
-use Chubbyphp\Tests\Deserialize\Resources\Model\One;
-use Chubbyphp\Tests\Deserialize\Resources\Model\Reference;
+use Chubbyphp\Tests\Deserialize\Resources\Mapping\SampleMapping;
+use Chubbyphp\Tests\Deserialize\Resources\Model\Sample;
 
 class DeserializerTest extends \PHPUnit_Framework_TestCase
 {
     public function testNew()
     {
-        $resolver = $this->getResolver();
-
         $objectMappingRegistry = new ObjectMappingRegistry([
-            new OneMapping(),
-            new ManyMapping($resolver),
-            new ReferenceMapping()
+            new SampleMapping()
         ]);
 
         $deserializer = new Deserializer($objectMappingRegistry);
 
-        /** @var One $one */
-        $one = $deserializer->deserializeByClass([
+        /** @var Sample $sample */
+        $sample = $deserializer->deserializeByClass([
             'name' => 'name1',
-            'unknownField' => 'dummy',
-            'manies' => [
-                0 => [
-                    'name' => 'name11',
-                    'reference' => [
-                        'name' => 'name111'
-                    ]
-                ],
-                1 => [
-                    'name' => 'name12',
-                    'reference' => [
-                        'name' => 'name121'
-                    ]
-                ]
-            ]
-        ], One::class);
+            'unknownField' => 'dummy'
+        ], Sample::class);
 
-        self::assertInstanceOf(One::class, $one);
-        self::assertNotNull($one->getId());
-        self::assertSame('name1', $one->getName());
-
-        $manies = $one->getManies();
-
-        self::assertCount(2, $manies);
-
-        /** @var Many $many1 */
-        $many1 = $manies[0];
-
-        self::assertInstanceOf(Many::class, $many1);
-        self::assertNotNull($many1->getId());
-        self::assertSame('name11', $many1->getName());
-        self::assertSame('name111', $many1->getReference()->getName());
-
-        /** @var Many $many2 */
-        $many2 = $manies[1];
-
-        self::assertInstanceOf(Many::class, $many2);
-        self::assertNotNull($many2->getId());
-        self::assertSame('name12', $many2->getName());
-        self::assertSame('name121', $many2->getReference()->getName());
+        self::assertInstanceOf(Sample::class, $sample);
+        self::assertNotNull($sample->getId());
+        self::assertSame('name1', $sample->getName());
     }
 
     public function testUpdate()
     {
-        $one = One::create();
-        $one->setName('name1');
-
-        $many1 = Many::create();
-        $many1->setName('name11');
-
-        $one->addMany($many1);
-
-        $many2 = Many::create();
-        $many2->setName('name12');
-
-        $one->addMany($many2);
-
-        $reference = Reference::create();
-        $reference->setName('name111');
-
-        $resolver = $this->getResolver([
-            Reference::class => [
-                $reference->getId() => $reference
-            ]
-        ]);
+        $sample = Sample::create();
+        $sample->setName('name1');
 
         $objectMappingRegistry = new ObjectMappingRegistry([
-            new OneMapping(),
-            new ManyMapping($resolver),
-            new ReferenceMapping()
+            new SampleMapping()
         ]);
 
         $deserializer = new Deserializer($objectMappingRegistry);
 
-        /** @var One $updatedOne */
-        $updatedOne = $deserializer->deserializeByObject([
-            'name' => 'name1',
-            'manies' => [
-                0 => [
-                    'name' => 'name11',
-                    'reference' => $reference->getId()
-                ],
-                2 => [
-                    'name' => 'name12',
-                    'reference' => null
-                ]
-            ]
-        ], $one);
+        /** @var Sample $updatedSample */
+        $updatedSample = $deserializer->deserializeByObject([
+            'name' => 'name2'
+        ], $sample);
 
-        self::assertSame($one, $updatedOne);
+        self::assertSame($sample, $updatedSample);
 
-        self::assertNotNull($updatedOne->getId());
-        self::assertSame('name1', $updatedOne->getName());
-
-        $manies = $updatedOne->getManies();
-
-        self::assertCount(2, $manies);
-
-        /** @var Many $updatedMany1 */
-        $updatedMany1 = $manies[0];
-
-        self::assertSame($many1, $updatedMany1);
-
-        self::assertInstanceOf(Many::class, $updatedMany1);
-        self::assertNotNull($updatedMany1->getId());
-        self::assertSame('name11', $updatedMany1->getName());
-        self::assertSame('name111', $updatedMany1->getReference()->getName());
-
-        /** @var Many $updatedMany2 */
-        $updatedMany2 = $manies[1];
-
-        self::assertNotSame($many2, $updatedMany2);
-
-        self::assertInstanceOf(Many::class, $updatedMany2);
-        self::assertNotNull($updatedMany2->getId());
-        self::assertSame('name12', $updatedMany2->getName());
-        self::assertNull($updatedMany2->getReference());
-    }
-
-    /**
-     * @return ResolverInterface
-     */
-    private function getResolver(array $models = []): ResolverInterface
-    {
-        /** @var ResolverInterface|\PHPUnit_Framework_MockObject_MockObject $resolver */
-        $resolver = $this->getMockBuilder(ResolverInterface::class)->setMethods(['find'])->getMockForAbstractClass();
-
-        $resolver->expects(self::any())->method('find')->willReturnCallback(
-            function (string $modelClass, string $id = null) use ($models) {
-                if (null === $id) {
-                    return null;
-                }
-
-                if (isset($models[$modelClass][$id])) {
-                    return $models[$modelClass][$id];
-                }
-            }
-        );
-
-        return $resolver;
+        self::assertNotNull($updatedSample->getId());
+        self::assertSame('name2', $updatedSample->getName());
     }
 }
--- tests/Resources/Mapping/SampleMapping.php
+++ tests/Resources/Mapping/SampleMapping.php
@@ -7,16 +7,16 @@ namespace Chubbyphp\Tests\Deserialize\Resources\Mapping;
 use Chubbyphp\Deserialize\Mapping\ObjectMappingInterface;
 use Chubbyphp\Deserialize\Mapping\PropertyMapping;
 use Chubbyphp\Deserialize\Mapping\PropertyMappingInterface;
-use Chubbyphp\Tests\Deserialize\Resources\Model\Reference;
+use Chubbyphp\Tests\Deserialize\Resources\Model\Sample;
 
-final class ReferenceMapping implements ObjectMappingInterface
+final class SampleMapping implements ObjectMappingInterface
 {
     /**
      * @return string
      */
     public function getClass(): string
     {
-        return Reference::class;
+        return Sample::class;
     }
 
     /**
