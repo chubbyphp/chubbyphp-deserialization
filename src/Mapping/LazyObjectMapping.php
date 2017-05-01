bd692f4cac5e1388684d974d35262bcdd02e6637
Dominik Zogg <dominik.zogg@gmail.com>
2017-05-01 10:29:45 +0200

    add LazyObjectMapping

---
+++ src/Mapping/LazyObjectMapping.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Deserialize\Mapping;
+
+use Interop\Container\ContainerInterface;
+
+final class LazyObjectMapping implements ObjectMappingInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var string
+     */
+    private $serviceId;
+
+    /**
+     * @var string
+     */
+    private $class;
+
+    /**
+     * @param ContainerInterface $container
+     * @param string             $serviceId
+     * @param string             $class
+     */
+    public function __construct(ContainerInterface $container, string $serviceId, string $class)
+    {
+        $this->container = $container;
+        $this->serviceId = $serviceId;
+        $this->class = $class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConstructMethod(): string
+    {
+        return $this->container->get($this->serviceId)->getConstructMethod();
+    }
+
+    /**
+     * @return PropertyMappingInterface[]
+     */
+    public function getPropertyMappings(): array
+    {
+        return $this->container->get($this->serviceId)->getPropertyMappings();
+    }
+}
