13f7aa654fee4dc1426dfdcaeb06006aec85a387
Dominik Zogg <dominik.zogg@gmail.com>
2017-09-27 08:21:49 +0200

    add decoder type tests

---
+++ tests/Decoder/JsonDecoderTypeTest.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Tests\Deserialization\Decoder;
+
+use Chubbyphp\Deserialization\Decoder\JsonDecoderType;
+
+class JsonDecoderTypeTest extends AbstractDecoderTypeTest
+{
+    public function testGetContentType()
+    {
+        $decoder = new JsonDecoderType();
+
+        self::assertSame('application/json', $decoder->getContentType());
+    }
+
+    /**
+     * @dataProvider getExpectedData
+     *
+     * @param array $expectedData
+     */
+    public function testDecode(array $expectedData)
+    {
+        $json = <<<EOD
+{
+    "page": 1,
+    "perPage": 10,
+    "search": null,
+    "sort": "name",
+    "order": "asc",
+    "_embedded": {
+        "mainItem": {
+            "id": "id1",
+            "name": "A fancy Name",
+            "treeValues": {
+                "1": {
+                    "2": 3
+                }
+            },
+            "progress": 76.8,
+            "active": true,
+            "_type": "item",
+            "_links": {
+                "read": {
+                    "href": "http://test.com/items/id1",
+                    "method": "GET"
+                },
+                "update": {
+                    "href": "http://test.com/items/id1",
+                    "method": "PUT"
+                },
+                "delete": {
+                    "href": "http://test.com/items/id1",
+                    "method": "DELETE"
+                }
+            }
+        },
+        "items": [
+            {
+                "id": "id1",
+                "name": "A fancy Name",
+                "treeValues": {
+                    "1": {
+                        "2": 3
+                    }
+                },
+                "progress": 76.8,
+                "active": true,
+                "_type": "item",
+                "_links": {
+                    "read": {
+                        "href": "http://test.com/items/id1",
+                        "method": "GET"
+                    },
+                    "update": {
+                        "href": "http://test.com/items/id1",
+                        "method": "PUT"
+                    },
+                    "delete": {
+                        "href": "http://test.com/items/id1",
+                        "method": "DELETE"
+                    }
+                }
+            },
+            {
+                "id": "id2",
+                "name": "B fancy Name",
+                "treeValues": {
+                    "1": {
+                        "2": 3,
+                        "3": 4
+                    }
+                },
+                "progress": 24.7,
+                "active": true,
+                "_type": "item",
+                "_links": {
+                    "read": {
+                        "href": "http://test.com/items/id2",
+                        "method": "GET"
+                    },
+                    "update": {
+                        "href": "http://test.com/items/id2",
+                        "method": "PUT"
+                    },
+                    "delete": {
+                        "href": "http://test.com/items/id2",
+                        "method": "DELETE"
+                    }
+                }
+            },
+            {
+                "id": "id3",
+                "name": "C fancy Name",
+                "treeValues": {
+                    "1": {
+                        "2": 3,
+                        "3": 4,
+                        "6": 7
+                    }
+                },
+                "progress": 100,
+                "active": false,
+                "_type": "item",
+                "_links": {
+                    "read": {
+                        "href": "http://test.com/items/id3",
+                        "method": "GET"
+                    },
+                    "update": {
+                        "href": "http://test.com/items/id3",
+                        "method": "PUT"
+                    },
+                    "delete": {
+                        "href": "http://test.com/items/id3",
+                        "method": "DELETE"
+                    }
+                }
+            }
+        ]
+    },
+    "_links": {
+        "self": {
+            "href": "http://test.com/items/?page=1&perPage=10&sort=name&order=asc",
+            "method": "GET"
+        },
+        "create": {
+            "href": "http://test.com/items/",
+            "method": "POST"
+        }
+    },
+    "_type": "search"
+}
+EOD;
+
+        $decoder = new JsonDecoderType();
+
+        self::assertEquals($expectedData, $decoder->decode($json));
+    }
+}
---
+++ tests/Decoder/XmlDecoderTypeTest.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Tests\Deserialization\Decoder;
+
+use Chubbyphp\Deserialization\Decoder\XmlDecoderType;
+
+class XmlDecoderTypeTest extends AbstractDecoderTypeTest
+{
+    public function testGetContentType()
+    {
+        $decoder = new XmlDecoderType();
+
+        self::assertSame('application/xml', $decoder->getContentType());
+    }
+
+    /**
+     * @dataProvider getExpectedData
+     *
+     * @param array $expectedData
+     */
+    public function testDecode(array $expectedData)
+    {
+        $xml = <<<EOD
+<?xml version="1.0" encoding="UTF-8"?>
+<object value="search">
+  <page type="integer">1</page>
+  <perPage type="integer">10</perPage>
+  <search></search>
+  <sort type="string">name</sort>
+  <order type="string">asc</order>
+  <meta-embedded>
+    <mainItem>
+      <object value="item">
+        <id type="string">id1</id>
+        <name type="string">A fancy Name</name>
+        <treeValues>
+          <treeValue key="1">
+            <treeValue type="integer" key="2">3</treeValue>
+          </treeValue>
+        </treeValues>
+        <progress type="float">76.8</progress>
+        <active type="boolean">true</active>
+        <meta-links>
+          <read>
+            <href type="string">http://test.com/items/id1</href>
+            <method type="string">GET</method>
+          </read>
+          <update>
+            <href type="string">http://test.com/items/id1</href>
+            <method type="string">PUT</method>
+          </update>
+          <delete>
+            <href type="string">http://test.com/items/id1</href>
+            <method type="string">DELETE</method>
+          </delete>
+        </meta-links>
+      </object>
+    </mainItem>
+    <items>
+      <object value="item" key="0">
+        <id type="string">id1</id>
+        <name type="string">A fancy Name</name>
+        <treeValues>
+          <treeValue key="1">
+            <treeValue type="integer" key="2">3</treeValue>
+          </treeValue>
+        </treeValues>
+        <progress type="float">76.8</progress>
+        <active type="boolean">true</active>
+        <meta-links>
+          <read>
+            <href type="string">http://test.com/items/id1</href>
+            <method type="string">GET</method>
+          </read>
+          <update>
+            <href type="string">http://test.com/items/id1</href>
+            <method type="string">PUT</method>
+          </update>
+          <delete>
+            <href type="string">http://test.com/items/id1</href>
+            <method type="string">DELETE</method>
+          </delete>
+        </meta-links>
+      </object>
+      <object value="item" key="1">
+        <id type="string">id2</id>
+        <name type="string">B fancy Name</name>
+        <treeValues>
+          <treeValue key="1">
+            <treeValue type="integer" key="2">3</treeValue>
+            <treeValue type="integer" key="3">4</treeValue>
+          </treeValue>
+        </treeValues>
+        <progress type="float">24.7</progress>
+        <active type="boolean">true</active>
+        <meta-links>
+          <read>
+            <href type="string">http://test.com/items/id2</href>
+            <method type="string">GET</method>
+          </read>
+          <update>
+            <href type="string">http://test.com/items/id2</href>
+            <method type="string">PUT</method>
+          </update>
+          <delete>
+            <href type="string">http://test.com/items/id2</href>
+            <method type="string">DELETE</method>
+          </delete>
+        </meta-links>
+      </object>
+      <object value="item" key="2">
+        <id type="string">id3</id>
+        <name type="string">C fancy Name</name>
+        <treeValues>
+          <treeValue key="1">
+            <treeValue type="integer" key="2">3</treeValue>
+            <treeValue type="integer" key="3">4</treeValue>
+            <treeValue type="integer" key="6">7</treeValue>
+          </treeValue>
+        </treeValues>
+        <progress type="float">100</progress>
+        <active type="boolean">false</active>
+        <meta-links>
+          <read>
+            <href type="string">http://test.com/items/id3</href>
+            <method type="string">GET</method>
+          </read>
+          <update>
+            <href type="string">http://test.com/items/id3</href>
+            <method type="string">PUT</method>
+          </update>
+          <delete>
+            <href type="string">http://test.com/items/id3</href>
+            <method type="string">DELETE</method>
+          </delete>
+        </meta-links>
+      </object>
+    </items>
+  </meta-embedded>
+  <meta-links>
+    <self>
+      <href type="string"><![CDATA[http://test.com/items/?page=1&perPage=10&sort=name&order=asc]]></href>
+      <method type="string">GET</method>
+    </self>
+    <create>
+      <href type="string">http://test.com/items/</href>
+      <method type="string">POST</method>
+    </create>
+  </meta-links>
+</object>
+EOD;
+
+        $decoder = new XmlDecoderType();
+
+        self::assertEquals($expectedData, $decoder->decode($xml));
+    }
+}
