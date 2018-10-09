abbd504057a2c7ec9412c8585c10745d2fba89b3
Dominik Zogg <dominik.zogg@gmail.com>
2018-10-09 20:47:49 +0200

    use \Closure::bind instead of reflections

--- src/Accessor/PropertyAccessor.php
+++ src/Accessor/PropertyAccessor.php
@@ -28,9 +28,21 @@ final class PropertyAccessor implements AccessorInterface
      */
     public function setValue($object, $value)
     {
-        $reflectionProperty = $this->getReflectionProperty($this->getClass($object));
-        $reflectionProperty->setAccessible(true);
-        $reflectionProperty->setValue($object, $value);
+        $class = $this->getClass($object);
+
+        if (!property_exists($class, $this->property)) {
+            throw DeserializerLogicException::createMissingProperty($class, $this->property);
+        }
+
+        $setter = \Closure::bind(
+            function ($property, $value) {
+                $this->$property = $value;
+            },
+            $object,
+            $class
+        );
+
+        return $setter($this->property, $value);
     }
 
     /**
@@ -40,10 +52,21 @@ final class PropertyAccessor implements AccessorInterface
      */
     public function getValue($object)
     {
-        $reflectionProperty = $this->getReflectionProperty($this->getClass($object));
-        $reflectionProperty->setAccessible(true);
+        $class = $this->getClass($object);
 
-        return $reflectionProperty->getValue($object);
+        if (!property_exists($class, $this->property)) {
+            throw DeserializerLogicException::createMissingProperty($class, $this->property);
+        }
+
+        $getter = \Closure::bind(
+            function ($property) {
+                return $this->$property;
+            },
+            $object,
+            $class
+        );
+
+        return $getter($this->property);
     }
 
     /**
@@ -62,19 +85,5 @@ final class PropertyAccessor implements AccessorInterface
         }
 
         return get_class($object);
-    }
-
-    /**
-     * @param string $class
-     *
-     * @return \ReflectionProperty
-     */
-    private function getReflectionProperty(string $class): \ReflectionProperty
-    {
-        try {
-            return new \ReflectionProperty($class, $this->property);
-        } catch (\ReflectionException $e) {
-            throw DeserializerLogicException::createMissingProperty($class, $this->property);
-        }
     }
 }
