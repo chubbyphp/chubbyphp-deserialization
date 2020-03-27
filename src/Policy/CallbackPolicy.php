a91c031498ca036cfdff5c2cd5ba58ff25344d10
Marko Ilic <marko.ilic@jobcloud.ch>
2020-03-27 17:18:17 +0100

    feat(JMP-1265): cs fix

--- src/Policy/CallbackPolicy.php
+++ src/Policy/CallbackPolicy.php
@@ -7,7 +7,7 @@ namespace Chubbyphp\Deserialization\Policy;
 use Chubbyphp\Deserialization\Denormalizer\DenormalizerContextInterface;
 
 /**
- * @deprecated Use CallbackPolicyIncludingPath instead
+ * @deprecated use CallbackPolicyIncludingPath instead
  */
 final class CallbackPolicy implements PolicyInterface
 {
@@ -23,7 +23,7 @@ final class CallbackPolicy implements PolicyInterface
 
     public function isCompliant(DenormalizerContextInterface $context, object $object): bool
     {
-        @trigger_error('Use "isCompliantIncludingPath()" instead of "isCompliant()"', E_USER_DEPRECATED);
+        @trigger_error('Use "CallbackPolicyIncludingPath" instead of "CallbackPolicy"', E_USER_DEPRECATED);
 
         return ($this->callback)($context, $object);
     }
